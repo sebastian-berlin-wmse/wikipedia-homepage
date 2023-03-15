23aed723ffca368c2d6c1a8433cd148f303e164a
Sebastian Berlin (WMSE) <sebastian.berlin@wikimedia.se>
2023-03-15 14:04:14 +0000

    Add banner to the top of the page
    Banner content can be loaded via endpoint /set-banner from the URL specified in the config. If specified, a CSS selector is used to get content from a specific element.
    
    Endpoint /preview-banner can be used to preview a banner without saving it. URL and selector can be specified through URL parameters.
    
    Config is loaded the first time for a request and then reused.
    
    Bug: T329613

--- app.py
+++ app.py
@@ -4,6 +4,7 @@ import yaml
 
 from flask import Flask
 from flask import abort
+from flask import g
 from flask import redirect
 from flask import render_template
 from flask import request
@@ -14,51 +15,58 @@ from flask_babel import _
 import requests
 from requests import Timeout
 from werkzeug.exceptions import HTTPException
+from bs4 import BeautifulSoup
+from bs4 import Comment
 
 app = Flask(__name__)
 babel = Babel(app)
 language = "sv"
 
 @app.route("/")
-def start():
+def start(banner=None):
     search_language = request.args.get("language")
-    with open("config.yaml") as config_file:
-        config = yaml.safe_load(config_file)
-
     if (
             not search_language or
-            config.get("search_languages").get(search_language) is None
+            config().get("search_languages").get(search_language) is None
     ):
-        search_language = "sv"
+        search_language = language
 
-    if "footer" in config:
-        footer = config["footer"]
-    else:
-        # Empty list instead of None to make the template code a bit
-        # cleaner.
-        footer = []
-
+    footer = config().get("footer", [])
     attributions = []
-    if "wikipedia_logo_attribution" in config:
-        attributions.append(config["wikipedia_logo_attribution"])
+    if "wikipedia_logo_attribution" in config():
+        attributions.append(config()["wikipedia_logo_attribution"])
     for block in footer:
         if "attribution" in block:
             attributions.append(block["attribution"])
 
-    search_language_parameters = config["search_languages"][search_language]
+    search_language_parameters = config()["search_languages"][search_language]
     search_language_parameters["code"] = search_language
     if "placeholder" not in search_language_parameters:
-        default_language = config["search_languages"][language]
+        default_language = config()["search_languages"][language]
         search_language_parameters["placeholder"] = default_language["placeholder"]
 
     return render_template(
         "index.html",
         lang=language,
-        search_languages=config["search_languages"],
+        search_languages=config()["search_languages"],
         search_language=search_language_parameters,
         footer=footer,
-        attributions=attributions
+        attributions=attributions,
+        banner=banner
     )
+
+def config():
+    """Get config
+
+    Loads config the first time this is used and saves it for later
+    access.
+
+    """
+    if g.get("config") is None:
+        with open("config.yaml") as config_file:
+            g.config = yaml.safe_load(config_file)
+
+    return g.config
 
 @app.route("/suggest")
 def suggest():
@@ -102,7 +110,49 @@ def go():
     url = f"http://{language}.wikipedia.org/wiki/{query}"
     return redirect(url)
 
+@app.route("/preview-banner")
+def preview():
+    url = request.args.get("url")
+    selector = request.args.get("selector")
+    banner_html = get_banner_html(url, selector)
+    return start(banner=banner_html)
+
+def get_banner_html(url=None, selector=None):
+    """Generate HTML for the banner
+
+    Uses url and selector from parameters if given and values from
+    config if not.
+
+    """
+    if url is None:
+        banner = config().get("banner", {})
+        url = banner.get("url")
+        if not url:
+            message = "Missing config variable <code>banner: url</code>."
+            return render_template("banner-error.html", message=message)
+
+        selector = banner.get("selector")
+
+    response = requests.get(url).text
+    if selector:
+        soup = BeautifulSoup(response, "html.parser")
+        html = soup.select(selector)[0]
+    else:
+        html = response
+
+    return html
+
+@app.route("/set-banner")
+def set_banner():
+    banner = config().get("banner", {})
+    url = banner.get("url")
+    selector = banner.get("selector")
+    html = get_banner_html(url, selector)
+    with open("templates/banner.html", "w") as f:
+        f.write(str(html))
+
+    return start()
+
 @babel.localeselector
 def get_locale():
     return language
-
